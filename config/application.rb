4c13da2f761d91b958d217cf776f57f9fdc4744c
pigutty <50047826+pigutty@users.noreply.github.com>
2019-04-28 17:31:06 +0900

    setting_of_haml_and_scss

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,18 @@
-!!!
-%html
-  %head
-    %meta{:content => "text/html; charset=UTF-8", "http-equiv" => "Content-Type"}/
-    %title ChatSpace
-    = stylesheet_link_tag    'application', media: 'all', 'data-turbolinks-track' => true
-    = javascript_include_tag 'application', 'data-turbolinks-track' => true
-    = csrf_meta_tags
-  %body
-    = render partial: 'layouts/flash'
-    = yield
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you’ve limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ChatSpace
+ class Application < Rails::Application
+   config.generators do |g|
+     g.stylesheets false
+     g.javascripts false
+     g.helper false
+     g.test_framework false
+   end
+ end
+end
