925546b10c3d8779497dd970e620f01a75f2c10d
pigutty <50047826+pigutty@users.noreply.github.com>
2019-05-03 14:20:43 +0900

    modify errors

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
  class Application < Rails::Application
+  config.action_view.field_error_proc = Proc.new do |html_tag, instance| 
+    html_tag
+  end
    config.generators do |g|
      g.stylesheets false
      g.javascripts false
