55b1dd82c1170f24429892dc84adc7fb1f46905d
pigutty <50047826+pigutty@users.noreply.github.com>
2019-05-20 22:52:57 +0900

    update automatic-update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,7 @@
-$(function(){
+$(document).on('turbolinks:load', function() {
+
+  var url = window.location.href;
+
   function buildHTML(message){
     var image = "";
     image = (message.image !== null)?  `<img src="${message.image}" class="content__message__image"></img>` : "" ;
@@ -15,14 +18,37 @@ $(function(){
     return html;
   }
 
-  var url = window.location.href;
+  $('#new_message').on('submit',function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var send_url = $(this).attr('action');
+    $.ajax({
+      url: send_url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.right-middle-content').append(html);
+      var height = $('.right-middle-content')[0].scrollHeight;
+      $('.right-middle-content').animate({scrollTop:height});
+      $('#message_content').val('');
+      $('.hidden').val('');
+    })
+    .fail(function(){
+      alert('メッセージの送信に失敗しました');
+    });
+  });
 
   var reloadMessages = function() {
     last_message_id = $('.message').eq(-1).attr('data-id');
     reload_url_pattern = /messages/;
-    result = url.replace(reload_url_pattern,'api/messages');
+    api_url = url.replace(reload_url_pattern,'api/messages');
     $.ajax({
-      url: result,
+      url: api_url,
       type: "GET",
       dataType: 'json',
       data: { last_id: last_message_id },
@@ -41,42 +67,17 @@ $(function(){
       $('.right-middle-content').animate({scrollTop:height});
     })
     .fail(function(){
-      console.log('自動更新に失敗しました');
+      alert('自動更新に失敗しました');
     })
   };
 
-  $('#new_message').on('submit',function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.right-middle-content').append(html);
-      var height = $('.right-middle-content')[0].scrollHeight;
-      $('.right-middle-content').animate({scrollTop:height});
-      $('#message_content').val('');
-      $('.hidden').val('');
-    })
-    .fail(function(){
-      console.log('メッセージの送信に失敗しました');
-    });
-    return false;
-  });
-
   var group_view_url_pattern = /\/groups\/\d+\/messages/;
   var result = url.match(group_view_url_pattern);
-  if (result) {
-    setInterval(reloadMessages, 5000);
+  var interval = setInterval(function() {
+  if (result){
+    reloadMessages();
   }
   else {
-    console.log('グループの表示画面でないです。');
-  }
+    clearInterval(interval);
+  }} ,5000)
 });
