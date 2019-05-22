b74871474cb9c3e39a355eb405722538408478d7
pigutty <50047826+pigutty@users.noreply.github.com>
2019-05-22 20:02:52 +0900

    Revert "Automatic update function"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def index
     @users = User.where('name LIKE(?)',"%#{params[:keyword]}%")
     respond_to do |format|
+      format.html
       format.json
     end
   end
